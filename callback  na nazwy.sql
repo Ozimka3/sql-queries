320bc0f417b46f5970897492889cf0c62cf2d8ae
Vadozimka <71928356+Vadozimka@users.noreply.github.com>
2021-10-13 12:31:21 +0200

    Update callback  na nazwy.sql

--- callback  na nazwy.sql
+++ callback  na nazwy.sql
@@ -77,5 +77,70 @@ AND bsk.ord_id_previous IS NOT NULL
 --AND (bsk.basket_label_en LIKE 'Amendment request%')
 
 
+--nn_account_autocompletion
+--USTAWIENIE KONTA wg typu Kosztu w danej spółce
+UPDATE alloc
+SET alloc.acc_code = la._acc_code
+	,alloc.lcomp_id = la._lcomp_id
+FROM t_ord_allocation AS alloc
+INNER JOIN x_orga_all x ON x.orga_level = alloc.orga_level AND x.orga_node = alloc.orga_node
+--Ledger Account (Konto) na podstawie Cost Type oraz Spółki.
+--Wybieramy jedno konto, i sprawdzamy ile ich jest
+OUTER APPLY
+(
+	SELECT MAX(la._acc_code) _acc_code, la._lcomp_id _lcomp_id, COUNT(1) ile
+	FROM t_ord_account_account_category_ la 
+	WHERE alloc.aca_code = la._aca_code 
+		AND la._lcomp_id = x.lcomp_id 
+		AND la.status_code = 'val'
+	GROUP BY la._aca_code, la._lcomp_id
+
+) la
+
+WHERE alloc.oitem_id=@oitem_id 
+	AND alloc.orga_level IS NOT NULL 
+	AND alloc.orga_node IS NOT NULL 
+	AND alloc.aca_code IS NOT NULL 
+	AND la.ile=1
+	
+--cost type 	
+	UPDATE alloc
+SET alloc.aca_code=bsk._aca_code
+FROM t_ord_allocation AS alloc
+LEFT JOIN t_ord_item oi ON oi.oitem_id=alloc.oitem_id
+LEFT JOIN t_ord_basket AS bsk ON bsk.basket_id=oi.basket_id
+WHERE alloc.oitem_id=@oitem_id 
+AND alloc.aca_code IS NULL
+AND bsk._aca_code IS NOT NULL
+AND oi.status_code !='del'
+AND bsk.status_code !='del'
+
+-- quick allocation 
+INSERT INTO t_ord_item_orga_all_
+( 
+  _oitem_id
+  ,_orga_id
+  )
+ SELECT 
+ oitem_id _oitem_id
+ ,boall._orga_id _orga_id
+ FROM 
+ t_ord_item AS oitem
+ JOIN t_ord_basket AS bsk ON bsk.basket_id = oitem.basket_id
+ JOIN t_ord_basket_orga_all_ AS boall ON boall._basket_id = bsk.basket_id
+ WHERE oitem.oitem_id = @oitem_id
+ AND (oitem._oitem_default_allocation IS NULL OR oitem._oitem_default_allocation = 0)
+ AND NOT EXISTS ( SELECT 1 FROM t_ord_item_orga_all_ AS ioall
+	 WHERE ioall._oitem_id = oitem.oitem_id AND ioall._orga_id = boall._orga_id)
+				 A
+--zaciagniecie wartosci domyslnych z nagłowka
+UPDATE oitem				 
+SET oitem._cce_code_cost_center_a = bsk.cce_code
+,oitem._aca_code_cost_type_b = bsk._aca_code
+,oitem._oitem_default_allocation = 1
+FROM t_ord_item AS oitem
+JOIN t_ord_basket AS bsk ON bsk.basket_id = oitem.basket_id
+ WHERE oitem.oitem_id = @oitem_id
+ AND (oitem._oitem_default_allocation IS NULL OR oitem._oitem_default_allocation = 0)
 
 
